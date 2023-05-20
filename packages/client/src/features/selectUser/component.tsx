09e6c1fb3d4efd65e90252af7426cdb0dd0fde80
Zica <jp.bahia.zica@gmail.com>
2023-05-20 12:43:48 -0300

    Início da Home

--- packages/client/src/features/selectUser/component.tsx
+++ packages/client/src/features/selectUser/component.tsx
@@ -1,31 +1,61 @@
+import { useState } from "react";
 import { userStore } from "../../stores/user/store";
-import { CreateUserContainer, UserList } from "./styles";
+import { CreateUserContainer, CreateUserField, UserList } from "./styles";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type CreateUserInputs = {
+  name: string;
+  email: string;
+};
 
 function SelectUser() {
   const allUsers = userStore((state) => state.all);
   const selectUser = userStore((state) => state.selectCurrentUser);
+  const [isCreatingUser, setIsCreatingUser] = useState(false);
+
+  const { register, handleSubmit } = useForm<CreateUserInputs>();
+  const onSubmit: SubmitHandler<CreateUserInputs> = (data) => console.log(data);
 
   return (
     <div>
       <h1>Selecione um usuário</h1>
       <div>
-        {allUsers.map((user) => (
-          <UserList>
-            <div>
-              {user.name} - {user.email}
-            </div>
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                selectUser(user);
-              }}
-            >Selecionar</button>
-          </UserList>
-        ))}
+        {allUsers.length ? (
+          allUsers.map((user) => (
+            <UserList>
+              <div>
+                {user.name} - {user.email}
+              </div>
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  selectUser(user);
+                }}
+              >
+                Selecionar
+              </button>
+            </UserList>
+          ))
+        ) : (
+          <UserList>Nenhum usuário encontrado</UserList>
+        )}
       </div>
-      <CreateUserContainer> 
-
-      </CreateUserContainer> 
+      <CreateUserContainer>
+        <button onClick={() => setIsCreatingUser((prev) => !prev)}>
+          {isCreatingUser ? "Fechar" : "Criar usuário"}
+        </button>
+        {isCreatingUser && (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <CreateUserField>
+              <input placeholder="Nome" {...register("name")} />
+            </CreateUserField>
+            <CreateUserField>
+              <input placeholder="Email" {...register("email")} />
+            </CreateUserField>
+            <button type="submit">Criar</button>
+          </form>
+        )}
+      </CreateUserContainer>
     </div>
   );
 }
--- packages/client/src/features/selectUser/styles.ts
+++ packages/client/src/features/selectUser/styles.ts
@@ -3,8 +3,23 @@ import styled from "styled-components";
 export const UserList = styled.div`
   display: flex;
   flex-flow: row;
+  margin: 20px 0;
 `;
 
 export const CreateUserContainer = styled.div`
+  margin: 10px 0;
 
+  form {
+    margin: 20px 0;
+  }
+`
+
+export const CreateUserField = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+
+  input {
+    margin: 10px 0;
+  }
 `
